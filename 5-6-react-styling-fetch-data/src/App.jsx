ca79f7e65141c84b975e9f8a396ba42f12cf3536
ReemaAlqahtani1 <s202280860@kfupm.edu.sa>
2026-03-04 11:45:17 +0300

    TODO 2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -202,9 +202,27 @@ export default function App() {
      ---------------------------------------------------------
      Implement fetch logic inside this useEffect.
      ========================================================= */
-  useEffect(() => {
+   useEffect(() => {
     // TODO 2.1: Implement fetching users here (see lab instructions)
-  }, []);
+      const fetchUsers = async () => {
+      try {
+         setLoading(true);
+         setError(null);
+         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+         if (!response.ok) {
+         throw new Error("Failed to fetch users");
+         }
+         const data = await response.json();
+         setUsers(data);
+         setFilteredUsers(data);
+      } catch (err) {
+         setError(err.message);
+      } finally {
+         setLoading(false);
+      }
+      };
+      fetchUsers();
+   }, []);
 
   /* =========================================================
      TODO 2.2 — FILTER USERS BY NAME
@@ -215,6 +233,15 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.2: Implement filtering users here (see lab instructions)
+      if (!searchTerm.trim()) {
+         setFilteredUsers(users);
+         return;
+      }
+      const term = searchTerm.toLowerCase();
+      const filtered = users.filter(user => 
+         user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredUsers(filtered);
   }, [searchTerm, users]);
 
   // Modal handlers (already complete)
